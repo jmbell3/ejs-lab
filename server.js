0d2c478c22f0b0ccef7c2c2cfb398ce9a735a1d6
jmbell3 <joelmichaelbell2@gmail.com>
2024-07-17 23:14:03 -0400

    comment2

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 // Sample restaurant data
 const RESTAURANT = {
   name: 'The Green Byte Bistro',
-  isOpen: true,
   address: '742 Evergreen Rd, Mapleview, OS 45502',
   phone: '555-321-9876',
   menu: [
@@ -13,7 +12,6 @@ const RESTAURANT = {
       name: 'Quantum Quinoa Mushroom Burger',
       price: 13.00,
       rating: 4,
-      category: 'mains',
       details: 'A vegetarian burger made with a quinoa and mushroom patty, it will take you to another realm.'
     },
     { 
@@ -21,7 +19,6 @@ const RESTAURANT = {
       name: 'Binary Berry Cheesecake',
       price: 10.11,
       rating: 3,
-      category: 'desserts',
       details: 'A creamy cheesecake bursting with flavor. A mix of berries in every byte.'
     },
     { 
@@ -29,7 +26,6 @@ const RESTAURANT = {
       name: 'Recursive Rigatoni',
       price: 17.00,
       rating: 5,
-      category: 'mains',
       details: 'A classic rigatoni pasta dish, layered with rich tomato sauce and herbs. You\'ll keep coming back for more.'
     },
     { 
@@ -37,7 +33,6 @@ const RESTAURANT = {
       name: 'Pumpkin Pi Squared',
       price: 3.14,
       rating: 5,
-      category: 'desserts',
       details: 'A delightful pumpkin dessert, squared and spiced to perfection.'
     },
     { 
@@ -45,12 +40,10 @@ const RESTAURANT = {
       name: 'Fibonacci String Bean Fries',
       price: 11.23,
       rating: 5,
-      category: 'sides',
       details: 'Crispy and lightly seasoned string bean fries, served in a pattern for a fun twist.'
     }
   ]
-}
-
+};
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
@@ -58,6 +51,11 @@ app.set('view engine', 'ejs');
 // Route to render the home page
 app.get('/', (req, res) => {
   res.render('home', { restaurant: RESTAURANT });
+});
+
+// Route to render the menu page
+app.get('/menu', (req, res) => {
+  res.render('menu', { menu: RESTAURANT.menu });
 });
 
 // Start server
